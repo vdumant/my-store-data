67b71a3e87dc90fad25683a8149cde7ab01dd772
duman <duman.fabian.g@gmail.com>
2022-07-04 15:29:01 -0500

    primer commit

---
+++ libs/postgres.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+const {config} = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+async function getConnection() {
+  const client = new Client({ connectionString: URI });
+
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,8 @@
 const faker = require('faker');
+const { Op } = require('sequelize');
 const boom = require('@hapi/boom');
+
+const { models } = require('../libs/sequelize');
 
 class ProductsService {
 
@@ -22,16 +25,35 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find(query) {
+    const options = {
+      include: ['category'],
+      where: {}
+    }
+    const { limit, offset } = query;
+    if (limit && offset) {
+      options.limit =  limit;
+      options.offset =  offset;
+    }
+
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
+    }
+    const products = await models.Product.findAll(options);
+    return products;
   }
 
   async findOne(id) {
